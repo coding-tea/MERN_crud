b037cbb960285706e284d4eb6a7ca1dcc53f846f
digital opium <test@ismail.com>
2023-06-26 22:23:14 +0100

    done <ith routes

---
+++ back-end/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+const connect = require('./db/connect');
+require('dotenv').config();
+connect(process.env.MONGO_URL);
+
+const dataRoutes = require('./routes/dataRoute');
+app.use('/data', )
