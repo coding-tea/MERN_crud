d613eaf3b5d3c30a986c4fbb26dd35a05dfa4138
digital opium <test@ismail.com>
2023-06-27 00:02:52 +0100

    start with front end

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+//dependencies
+
+//css
+import './app.css';
+
+function App() {
+
+
+  return (
+    <div className='app'>
+      <header>
+          react router dom app <a href="https://github.com/coding-tea" target='_blank' rel="noreferrer">@codingTea <i class="bi bi-github"></i></a>
+      </header>
+      <div className="table">
+        <table>
+          <tr>
+            <th> # </th>
+            <th> name </th>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
